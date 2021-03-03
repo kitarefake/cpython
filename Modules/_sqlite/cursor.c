kitarefake/cpython

commit 8ac6a36e97c2bc1e747c62d818c7b5ab4ffd6976

bpo-43249: sqlite3_column_bytes() must follow sqlite_column_blob() (GH-24562)

Modules/_sqlite/cursor.c
@@ -278,9 +278,15 @@ _pysqlite_fetch_one_row(pysqlite_Cursor* self)
             converter = Py_None;
         }
 
+        /*
+         * Note, sqlite3_column_bytes() must come after sqlite3_column_blob()
+         * or sqlite3_column_text().
+         *
+         * See https://sqlite.org/c3ref/column_blob.html for details.
+         */
         if (converter != Py_None) {
-            nbytes = sqlite3_column_bytes(self->statement->st, i);
             val_str = (const char*)sqlite3_column_blob(self->statement->st, i);
+            nbytes = sqlite3_column_bytes(self->statement->st, i);
             if (!val_str) {
                 converted = Py_NewRef(Py_None);
             } else {
@@ -330,9 +336,13 @@ _pysqlite_fetch_one_row(pysqlite_Cursor* self)
                 }
             } else {
                 /* coltype == SQLITE_BLOB */
-                nbytes = sqlite3_column_bytes(self->statement->st, i);
-                converted = PyBytes_FromStringAndSize(
-                    sqlite3_column_blob(self->statement->st, i), nbytes);
+                const char *blob = sqlite3_column_blob(self->statement->st, i);
+                if (!blob) {
+                    converted = Py_NewRef(Py_None);
+                } else {
+                    nbytes = sqlite3_column_bytes(self->statement->st, i);
+                    converted = PyBytes_FromStringAndSize(blob, nbytes);
+                }
             }
         }
 

commit a40e9c51af3f2da744ee42fb95d5da7566b412ab

bpo-43368: Fix fetching empty bytes in sqlite3 (GH-24706)
Regression introduced in 8ac6a36e97c2bc1e747c62d818c7b5ab4ffd6976.

Modules/_sqlite/cursor.c
@@ -333,12 +333,8 @@ _pysqlite_fetch_one_row(pysqlite_Cursor* self)
             } else {
                 /* coltype == SQLITE_BLOB */
                 const char *blob = sqlite3_column_blob(self->statement->st, i);
-                if (!blob) {
-                    converted = Py_NewRef(Py_None);
-                } else {
-                    nbytes = sqlite3_column_bytes(self->statement->st, i);
-                    converted = PyBytes_FromStringAndSize(blob, nbytes);
-                }
+                nbytes = sqlite3_column_bytes(self->statement->st, i);
+                converted = PyBytes_FromStringAndSize(blob, nbytes);
             }
         }
 

Lib/sqlite3/test/regression.py
@@ -409,6 +409,10 @@ class RegressionTests(unittest.TestCase):
             self.con.execute("select 1")  # trigger seg fault
             method(None)
 
+    def test_return_empty_bytestring(self):
+        cur = self.con.execute("select X''")
+        val = cur.fetchone()[0]
+        self.assertEqual(val, b'')
 
 
 def suite():